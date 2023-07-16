33aea9f84babca8778f64daa61c648f4a713c390
deokgoo <kkddgg1001@gmail.com>
2023-07-16 18:51:17 +0900

    init proejct

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+const Home = () => {
+  return (
+    <main className="w-screen h-screen flex flex-col">
+      <div className="h-20 bg-red-500">Flex Item 1 (30%)</div>
+      <div className="h-60 bg-red-500">Flex Item 1 (60%)</div>
+      <div className="h-20 bg-red-500">Flex Item 1 (30%)</div>
+    </main>
+  );
+};
+
+export default Home;
